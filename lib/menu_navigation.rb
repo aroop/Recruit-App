d03c0bd60ccd81864314b28b18cc4e37f3fd8e0a
Ajay Kumar G <ajay@aapamtech.com>
2009-06-14 12:48:29 -0700

    Changed Logos Added index pages stubs

--- lib/menu_navigation.rb
+++ lib/menu_navigation.rb
@@ -4,9 +4,9 @@ module MenuNavigation
     @menu_navigation = []
     @menu_navigation << MenuItem.new("Dashboard", dashboard_path)
     @menu_navigation << contacts_menu
-    @menu_navigation << MenuItem.new("Companies", companies_path)
-    @menu_navigation << MenuItem.new("Candidates", candidates_path)
-    @menu_navigation << MenuItem.new("Jobs", jobs_path)
+    @menu_navigation << companies_menu
+    @menu_navigation << candidates_menu
+    @menu_navigation << jobs_menu
     @menu_navigation << MenuItem.new("Messages", messages_path)
     @menu_navigation << MenuItem.new("Analytics", "")
     @menu_navigation << MenuItem.new("Accounts", "")
@@ -17,6 +17,27 @@ module MenuNavigation
     contact_menu.children << MenuItem.new("Add new", new_contact_path, "sm1")
     contact_menu.children << MenuItem.new("List", contacts_path, "sm2")
     contact_menu
+  end
+  
+  def companies_menu
+    companies_menu = MenuItem.new("Companies", companies_path)
+    companies_menu.children << MenuItem.new("Add new", new_company_path, "sm1")
+    companies_menu.children << MenuItem.new("List", companies_path, "sm2")
+    companies_menu
+  end
+  
+  def candidates_menu
+    candidates_menu = MenuItem.new("Candidates", candidates_path)
+    candidates_menu.children << MenuItem.new("Add new", new_candidate_path, "sm1")
+    candidates_menu.children << MenuItem.new("List", candidates_path, "sm2")    
+    candidates_menu
+  end
+  
+  def jobs_menu
+    jobs_menu = MenuItem.new("Jobs", jobs_path)
+    jobs_menu.children << MenuItem.new("Add new", new_job_path, "sm1")
+    jobs_menu.children << MenuItem.new("List", jobs_path, "sm2")
+    jobs_menu
   end
   
 end
